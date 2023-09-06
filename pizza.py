cb919eea7a32a2ef6b8c8efe6a4004909ef25f84
LENI4C <timibraperetei74@gmail.com>
2023-09-06 17:00:35 +0100

    made price of toppings change with chosen choiceIndex

--- pizza.py
+++ pizza.py
@@ -1,15 +1,11 @@
 #TODO:
 # Allow customer to specify size, and choice of toppings
 #include option of supreme for each size
-# diable individual selection if supreme is selected
+# disable individual selection if supreme is selected
 # display cost of pizza, tax : 6%, then total cost
 # confirm project works
 
 hasToppings = True
-
-toppingSmall = 1
-toppingMedium = 1.50
-toppingLarge = 2.25
 
 toppings = [
     {"mushrooms" : 1},
@@ -25,43 +21,69 @@ pizzaType =  [
     {"small pizza" : [10]}, 
     {"medium pizza" : [12]}, 
     {"large pizza" : [15]},
-
     {"supreme small" : [15, hasToppings ]},
     {"supreme medium" : [20, hasToppings ]},
     {"supreme large" : [27, hasToppings ]}
 ]
 
-print("\nALL PIZZAS EXCEPT THE SUPREME PACKAGE COMES PLAIN WITH CHEESE AND TOMATO SAUCE \n\nTHERE ARE CHOICES FOR OPTIONAL TOPPINGS THOUGH")
-
+print("\nALL PIZZAS EXCEPT THE SUPREME PACKAGE COMES PLAIN WITH CHEESE AND TOMATO SAUCE \n\nTHERE'S OPTION FOR TOPPINGS THOUGH")
 
 def pizzaAlgorithm():
     i = 0
-    # interesting thingy going on here, i set the first item in the list to be an empty string so that when user selects 1 it'll not give them medium pizza instead of the ideal small pizza which it should've given them
+    # interesting thingy going on here, I set the first item in the list to be an empty string so that when user selects 1 it'll not give them medium pizza instead of the ideal small pizza which it should've given them
     options = ["",]
     optionPrices = ["",]
     for pizza in pizzaType:
         i+=1
         for choice, attr in pizza.items():
-            print(f"{i}) {choice} : {attr[0]}$")
+            print(f"{i}) {choice} : ${attr[0]}")
             options.append(choice)
             optionPrices.append(attr[0])
 
-    ChoiceIndex = int(input("\nWHAT KIND OF PIZZA WOULD YOU LIKE : \nSELECT BY ENTERING THE NUMBER OF THE PIZZA: "))
+    choiceIndex = int(input("\nWHAT KIND OF PIZZA WOULD YOU LIKE? \nSELECT BY ENTERING THE NUMBER OF THE PIZZA: "))
+    pizzaName = options[choiceIndex]
+    pizzaPrice = optionPrices[choiceIndex]
+    tax = 0.06 * pizzaPrice
+    grossPrice = tax + pizzaPrice
+    orderReconfirmation = f""
+    orderCost = f""
+    order = f""
+    closingRemark = f""
 
-    if (ChoiceIndex > 0 and ChoiceIndex < 4):       
-        print(f"\nYou have chosen the {options[ChoiceIndex].upper()}, and you are eligible to get toppings") 
+    if (choiceIndex > 0 and choiceIndex < 4):       
+        print(f"\nYou have chosen the {pizzaName.upper()}, and you are eligible to get toppings") 
 
         wantToppings = input("\nDO YOU WANT TOPPINGS WITH YOUR PIZZA? (Y/N): ").lower()
 
         if (wantToppings == "y" or wantToppings == "yes" ):
             print("\nTOPPINGS")
-            for topping in toppings:
-                for choice, attr in topping.items():
-                    print(f"{choice} : {attr}$")
-        elif (wantToppings == "n" or wantToppings == "no" ):
-            print(f"\nYou have opted to have your {options[ChoiceIndex].upper()} without any toppings\nThat'll cost you {optionPrices[ChoiceIndex]}$ \n\nYOUR ORDER: \n{options[ChoiceIndex].upper()} : {optionPrices[ChoiceIndex]}$ \n\nTHANK YOU FOR SHOPPING WITH US !!! ")
+            if (choiceIndex == 1):
+                for topping in toppings:
+                    for choice, attr in topping.items():
+                        print(f"{choice} : ${attr}")
+            elif (choiceIndex == 2):
+                for topping in toppings:
+                    for choice, attr in topping.items():
+                        print(f"{choice} : ${attr * 1.5}")
+            else:
+                for topping in toppings:
+                    for choice, attr in topping.items():
+                        print(f"{choice} : ${attr * 2.25}")
 
-            print(f"")
+
+        elif (wantToppings == "n" or wantToppings == "no" ):
+
+            orderReconfirmation = f"\nYou have opted to have your {pizzaName.upper()} without any toppings"
+
+            orderCost = f"\nThat'll cost you ${grossPrice}"
+
+            order = f"\n\nYOUR ORDER: \n{pizzaName.upper()} : ${pizzaPrice} \nTAX: ${tax} (STANDARD 6% VAT)"
+            
+            closingRemark = f"\n\nTHANK YOU FOR SHOPPING WITH US !!!"
+
+            print(f"{orderReconfirmation}{orderCost}{order}{closingRemark}")
+
+            
 
 
 pizzaAlgorithm()
