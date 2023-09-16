9aa44acbaacfe4368339ab8295347ea67d0421ac
LENI4C <timibraperetei74@gmail.com>
2023-09-16 21:43:47 +0100

    small pizza fully functional

--- pizza.py
+++ pizza.py
@@ -7,6 +7,15 @@
 
 hasToppings = True
 
+pizzaType =  [
+    {"small pizza" : [10]}, 
+    {"medium pizza" : [12]}, 
+    {"large pizza" : [15]},
+    {"supreme small" : [15, hasToppings ]},
+    {"supreme medium" : [20, hasToppings ]},
+    {"supreme large" : [27, hasToppings ]}
+]
+
 toppings = [
     {"mushrooms" : 1},
     {"pepperoni" : 1},
@@ -17,16 +26,7 @@ toppings = [
     {"shrimp" : 1}
 ]
 
-pizzaType =  [
-    {"small pizza" : [10]}, 
-    {"medium pizza" : [12]}, 
-    {"large pizza" : [15]},
-    {"supreme small" : [15, hasToppings ]},
-    {"supreme medium" : [20, hasToppings ]},
-    {"supreme large" : [27, hasToppings ]}
-]
-
-print("\nALL PIZZAS EXCEPT THE SUPREME PACKAGE COMES PLAIN WITH CHEESE AND TOMATO SAUCE \n\nTHERE'S OPTION FOR TOPPINGS THOUGH")
+print("\nALL PIZZAS EXCEPT THE SUPREME PACKAGE COMES PLAIN WITH CHEESE AND TOMATO SAUCE \n\nTHERE'S OPTION FOR TOPPINGS THOUGH\n")
 
 def pizzaAlgorithm():
     i = 0
@@ -51,16 +51,58 @@ def pizzaAlgorithm():
     closingRemark = f""
 
     if (choiceIndex > 0 and choiceIndex < 4):       
-        print(f"\nYou have chosen the {pizzaName.upper()}, and you are eligible to get toppings") 
+        print(f"\nYou have chosen the {pizzaName.upper()},") 
 
-        wantToppings = input("\nDO YOU WANT TOPPINGS WITH YOUR PIZZA? (Y/N): ").lower()
+        wantToppings = input("DO YOU WANT TOPPINGS WITH YOUR PIZZA? (Y/N): ").lower()
 
+        # TODO : make the yes option able to add price of each token
         if (wantToppings == "y" or wantToppings == "yes" ):
+            i = 0
             print("\nTOPPINGS")
+            toppingChoice = []
+            toppingName = ["",]
+            toppingPrice = ["",]
+            top = ""
+
             if (choiceIndex == 1):
                 for topping in toppings:
+                    i+=1
                     for choice, attr in topping.items():
-                        print(f"{choice} : ${attr}")
+                        print(f"{i}) {choice} : ${attr}")
+                        toppingName.append(choice)
+                        toppingPrice.append(attr)
+
+                while top != "done" : 
+                    top = input("\nWHAT TOPPING WOULD YOU LIKE? \nSELECT BY ENTERING THE NUMBER OF THE TOPPING (TO END SELECTION ENTER \"done\"): ").lower()
+                    toppingChoice.append(top)
+
+                toppingChoice.pop()
+                
+                # CHAT GPT'S CODE #STARTS HERE
+                chosenToppings = [toppingName[int(tops)] for tops in toppingChoice if 1 <= int(tops) <= len(toppingName) - 1]
+
+                if chosenToppings:
+                    if len(chosenToppings) == 1:
+                        print("You have chosen " + chosenToppings[0])
+                    else:
+                        notLastTopping = ", ".join(chosenToppings[:-1])
+                        lastTopping = chosenToppings[-1]
+                        print(f"You have chosen {notLastTopping}, and {lastTopping}")
+                    orderReconfirmation = f"\nYou have opted to have {pizzaName.upper()} with {notLastTopping}, and {lastTopping}"
+
+                    grossPrice += toppingPrice[1] * int(len(chosenToppings)) 
+                    orderCost = f"\nThat'll cost you ${grossPrice}"
+
+                    order = f"\n\nYOUR ORDER: \n{pizzaName.upper()} : ${pizzaPrice} \nTOPPINGS: {notLastTopping}, and {lastTopping} (EACH COST ${toppingPrice[1]})  \nTAX: ${tax} (STANDARD 6% VAT)"
+                    
+                    closingRemark = f"\n\nTHANK YOU FOR SHOPPING WITH US !!!"
+
+                    print(f"{orderReconfirmation}{orderCost}{order}{closingRemark}")
+                else:
+                    print("No toppings chosen.")
+
+                # CHAT GPT'S CODE ENDS HERE
+
             elif (choiceIndex == 2):
                 for topping in toppings:
                     for choice, attr in topping.items():
